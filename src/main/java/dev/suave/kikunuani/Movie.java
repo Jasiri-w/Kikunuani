ecee0a46571464001c024fda762694dec4d9b75b
Jasiri M. Wa-Kyendo <jasiri.w@gmail.com>
2023-06-06 09:01:52 +0000

    Starting Afresh

--- src/main/java/dev/suave/kikunuani/Movie.java
+++
@@ -1,9 +0,0 @@
-package dev.suave.kikunuani;
-
-import org.springframework.data.mongodb.core.mapping.Document;
-
-@Document(collection = "movies")
-public class Movie {
-    private Integer id;
-    private String imdbId;
-}
